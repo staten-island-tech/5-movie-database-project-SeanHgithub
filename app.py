52fa334b47693fefa6638e6066e7dc2581451983
SeanHgithub <seanhadley11@gmail.com>
2025-10-24 09:49:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
+""" for i in data:
+    print(i["title"]) """
+user_input = int(input("What is the oldest year that you would watch a movie from?"))
+user_input2 = int(input("What is the most recent year that you would watch a movie from?"))
+for movie in data:
+    if movie["year"] >= user_input and movie["year"] <= user_input2: 
+        print(movie["title"])
