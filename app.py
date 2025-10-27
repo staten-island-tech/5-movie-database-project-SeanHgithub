7ba11dea954c04fdbcbac4e2d187ebf86c98116a
SeanHgithub <seanhadley11@gmail.com>
2025-10-27 10:16:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,18 @@ data = json.load(movies)
 
 """ for i in data:
     print(i["title"]) """
-user_input = int(input("What is the oldest year that you would watch a movie from?"))
+""" user_input = int(input("What is the oldest year that you would watch a movie from?"))
 user_input2 = int(input("What is the most recent year that you would watch a movie from?"))
 for movie in data:
     if movie["year"] >= user_input and movie["year"] <= user_input2: 
-        print(movie["title"])
+        print(movie["title"]) """
+""" user_input = int(input("What year do you want to watch a move from?"))
+for movie in data:
+    if movie["year"] == user_input:
+        print(movie["title"])   """
+""" user_input = input("What movie do you want to watch?")
+for movie in data:
+   if user_input == movie["title"]:
+      print(movie["title"]) """
+user_input = input("What genre would you like to watch?")
+
